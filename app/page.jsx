e7ca122fd1f447f93997ce663de06a11f0729599
Voo Tzuen Hau <vince_voo@yahoo.com>
2023-09-15 22:43:50 +0800

    completed

--- app/page.jsx
+++ app/page.jsx
@@ -4,13 +4,17 @@ const Home = () => {
   return (
     <section className="w-full flex-center flex-col">
       <h1 className="head_text text-center">
-        Discover & Share
+        Uncover & Share
         <br className="max-md:hidden" />
-        <span className="orange_gradient text-center"> AI-Powered Prompts</span>
+        <span className="orange_gradient text-center">
+          {" "}
+          AI-Generated Prompts
+        </span>
       </h1>
       <p className="desc text-center">
-        PromptVerse is an open-source AI prompting tool for modern world to
-        discover, create and share creative prompts.
+        Designed for the modern world, PromptVerse serves as an open-source AI
+        prompt utility, allowing users to explore, craft, and disseminate
+        imaginative prompts.
       </p>
 
       <Feed />
